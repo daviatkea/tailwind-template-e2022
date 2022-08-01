9292dbcc1e56233cc91e0278490ee04fe11991f6
Dannie Vinther <davi@kea.dk>
2022-08-01 18:48:38 +0200

    src folder

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,vue,js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,17 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  root: resolve(__dirname, "src"),
   base: "./",
   preview: {
     host: true,
   },
+  publicDir: resolve("public"),
   build: {
+    outDir: resolve("dist"),
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
+        main: resolve(__dirname, "src/index.html"),
         // about: resolve(__dirname, "about.html"),
       },
     },
